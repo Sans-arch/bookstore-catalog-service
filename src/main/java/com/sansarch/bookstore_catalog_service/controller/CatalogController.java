70991cdf7b0e5417606f499eddd4c02f37437499
Santiago Negreira <sansfleitas@gmail.com>
2024-11-26 17:14:13 -0300

    Create CRUD endpoints

--- src/main/java/com/sansarch/bookstore_catalog_service/controller/CatalogController.java
+++ src/main/java/com/sansarch/bookstore_catalog_service/controller/CatalogController.java
@@ -1,37 +1,38 @@
 package com.sansarch.bookstore_catalog_service.controller;
 
-import com.sansarch.bookstore_catalog_service.dto.CreateBookInputDto;
-import com.sansarch.bookstore_catalog_service.dto.CreateBookOutputDto;
+import com.sansarch.bookstore_catalog_service.dto.*;
 import com.sansarch.bookstore_catalog_service.service.CatalogService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/catalog")
 public class CatalogController {
 
     private CatalogService catalogService;
 
     @PostMapping
-    public CreateBookOutputDto addBook(CreateBookInputDto input) {
-        return catalogService.addBookToCatalog(input);
+    public ResponseEntity<CreateBookOutputDto> addBook(@RequestBody CreateBookInputDto input) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(catalogService.addBookToCatalog(input));
     }
 
-//    // list all books
-//    @GetMapping
-//    public void listAllBooks() {
-//
-//    }
-//
-//    // get details of specific book
-//    @GetMapping("/${id}")
-//    public void getBook(@PathVariable Long id) {
-//
-//    }
-//
-//    // update book details - put
-//    @PutMapping("/${id}")
-//    public void updateBook(@PathVariable Long id) {
-//
-//    }
+    @GetMapping
+    public ResponseEntity<List<ListBooksOutputDto>> listAllBooks() {
+        return ResponseEntity.ok(catalogService.listAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FindBookOutputDto> getBook(@PathVariable Long id) {
+        return ResponseEntity.ok(catalogService.findBook(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EditBookOutputDto> editBook(@PathVariable Long id, @RequestBody EditBookInputDto input) {
+        return ResponseEntity.ok(catalogService.editBook(id, input));
+    }
 }
--- src/main/java/com/sansarch/bookstore_catalog_service/service/CatalogService.java
+++ src/main/java/com/sansarch/bookstore_catalog_service/service/CatalogService.java
@@ -1,12 +1,14 @@
 package com.sansarch.bookstore_catalog_service.service;
 
-import com.sansarch.bookstore_catalog_service.dto.CreateBookInputDto;
-import com.sansarch.bookstore_catalog_service.dto.CreateBookOutputDto;
+import com.sansarch.bookstore_catalog_service.dto.*;
 import com.sansarch.bookstore_catalog_service.entity.Book;
+import com.sansarch.bookstore_catalog_service.exception.BookNotFoundException;
 import com.sansarch.bookstore_catalog_service.mapper.BookMapper;
 import com.sansarch.bookstore_catalog_service.repository.BookRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
@@ -17,7 +19,26 @@ public class CatalogService {
     public CreateBookOutputDto addBookToCatalog(CreateBookInputDto input) {
         Book book = BookMapper.INSTANCE.createBookInputDtoToBookEntity(input);
         bookRepository.save(book);
-
         return BookMapper.INSTANCE.bookEntityToCreateBookOutputDto(book);
+    }
+
+    public List<ListBooksOutputDto> listAllBooks() {
+        return BookMapper.INSTANCE.bookEntityListToListBooksOutputDtoList(bookRepository.findAll());
+    }
+
+    public FindBookOutputDto findBook(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException(id));
+        return BookMapper.INSTANCE.bookEntityToFindBookOutputDto(book);
+    }
+
+    public EditBookOutputDto editBook(Long id, EditBookInputDto input) {
+        var book = this.bookRepository.findById(id).orElseThrow(() -> new BookNotFoundException(id));
+
+        book.setTitle(input.getTitle());
+        book.setAuthor(input.getAuthor());
+        book.setStockAvailability(input.getStockAvailability());
+        this.bookRepository.save(book);
+
+        return BookMapper.INSTANCE.bookEntityToEditBookOutputDto(book);
     }
 }
