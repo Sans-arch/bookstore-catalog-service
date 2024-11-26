b175d85ab4ab827bf5c0c413f6c90ba14b6bc4ae
Santiago Negreira <sansfleitas@gmail.com>
2024-11-26 17:14:39 -0300

    Config mappings for endpoints usecases dto's

--- src/main/java/com/sansarch/bookstore_catalog_service/mapper/BookMapper.java
+++ src/main/java/com/sansarch/bookstore_catalog_service/mapper/BookMapper.java
@@ -1,11 +1,12 @@
 package com.sansarch.bookstore_catalog_service.mapper;
 
-import com.sansarch.bookstore_catalog_service.dto.CreateBookInputDto;
-import com.sansarch.bookstore_catalog_service.dto.CreateBookOutputDto;
+import com.sansarch.bookstore_catalog_service.dto.*;
 import com.sansarch.bookstore_catalog_service.entity.Book;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper
 public interface BookMapper {
@@ -23,4 +24,22 @@ public interface BookMapper {
     @Mapping(source = "author", target = "author")
     @Mapping(source = "stockAvailability", target = "stockAvailability")
     CreateBookOutputDto bookEntityToCreateBookOutputDto(Book book);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "author", target = "author")
+    @Mapping(source = "stockAvailability", target = "stockAvailability")
+    List<ListBooksOutputDto> bookEntityListToListBooksOutputDtoList(List<Book> books);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "author", target = "author")
+    @Mapping(source = "stockAvailability", target = "stockAvailability")
+    FindBookOutputDto bookEntityToFindBookOutputDto(Book book);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "title", target = "title")
+    @Mapping(source = "author", target = "author")
+    @Mapping(source = "stockAvailability", target = "stockAvailability")
+    EditBookOutputDto bookEntityToEditBookOutputDto(Book book);
 }
