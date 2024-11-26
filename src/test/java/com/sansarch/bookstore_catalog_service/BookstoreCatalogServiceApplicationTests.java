7ec34f4c8661ca1f3e0b9f0120ccf9e6d5d056fb
Santiago Negreira <sansfleitas@gmail.com>
2024-11-26 10:40:44 -0300

    Initialize project

---
+++ src/test/java/com/sansarch/bookstore_catalog_service/BookstoreCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansarch.bookstore_catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
