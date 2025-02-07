b82d61ce79c9bb8fe0c69b1babac68e86bb5afb9
Santiago Negreira <sansfleitas@gmail.com>
2025-02-07 12:36:50 -0300

    Add Swagger (openapi) docs and config it

---
+++ src/main/java/com/sansarch/bookstore_catalog_service/infra/http/config/OpenAPIConfig.java
@@ -0,0 +1,20 @@
+package com.sansarch.bookstore_catalog_service.infra.http.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Catalog Service API")
+                        .version("1.0")
+                        .description("API for managing books in the bookstore")
+                );
+    }
+}
